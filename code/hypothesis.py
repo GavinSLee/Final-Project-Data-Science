0d75c4ade0cbce3f3f46615bbd8d215e9a3d0a5b
andrewdkim <adkim414@gmail.com>
2022-04-17 18:51:23 -0400

    viralities per month

--- code/hypothesis.py
+++ code/hypothesis.py
@@ -32,7 +32,7 @@ Hypothesis #3a
 What COVID keywords do the tweets need to contain in order for the tweet to have a high virality number?
 For each keyword, Calculate number of posts per each range that contained that specific keyword
 """
-def keywords_with_high_virality():
+def keywords_with_virality():
     cnn_tweets_read_path = "../data_clean/cnn_tweets_clean.json"
     fox_tweets_read_path = "../data_clean/fox_tweets_clean.json"
     out_path = "../data_clean/keywords_viralities.json"
@@ -58,10 +58,43 @@ Hypothesis #3b
 Are there certain months in which tweeting with a certain COVID keyword will lead to a high virality number? 
 """
 
-def keywords_with_high_virality_per_month():
-    cnn_replies_read_path = "../data_clean/cnn_replies_clean.json"
+def keywords_with_virality_per_month():
+    cnn_tweets_read_path = "../data_clean/cnn_tweets_clean.json"
     fox_tweets_read_path = "../data_clean/fox_tweets_clean.json"
     out_path = "../data_clean/keywords_viralities_per_month.json"
+    months_2020 = ["January 2020", "February 2020", "March 2020", "April 2020", "May 2020", "June 2020", "July 2020", "August 2020", "September 2020", "October 2020", "November 2020", "December 2020"]
+    months_2021 = ["January 2021", "February 2021", "March 2021", "April 2021", "May 2021", "June 2021", "July 2021", "August 2021", "September 2021", "October 2021", "November 2021", "December 2021"]
+    months = months_2020 + months_2021
+    all_tweets = load_json(cnn_tweets_read_path) + load_json(fox_tweets_read_path)
+    result = {}
+    for keyword in terms:
+        data = {}
+        for m in months:
+            data[m] = {"total": 0, "count": 0}
+        for tweet in all_tweets:
+            if keyword not in tweet["keywords"]: continue
+            month = tweet["month"]
+            virality = tweet_virality(tweet)
+            data[month]["total"] += virality
+            data[month]["count"] += 1
+        total_count = 0 #this is for tracking if keyword has been used 
+        for month, datum in data.items():
+            if datum["count"] == 0:
+                data[month] = 0
+            else:
+                total_count += datum["count"]
+                data[month] = datum["total"] / datum["count"]
+        if total_count == 0: continue
+        result[keyword] = data
+    save_json(out_path, result)
+            
+
+        
+        
+
+            
+        
+
 
     
 
@@ -70,7 +103,7 @@ def keywords_with_high_virality_per_month():
 
 
 def main():
-    keywords_with_high_virality()
+    keywords_with_virality_per_month()
 
 if __name__ == "__main__":
     main() 
